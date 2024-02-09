1bb3248ff1b9b8da2f3bc3c9309364190ddaa3b3
MatteoSanson <matteo.sanson@gmail.com>
2024-02-09 18:37:52 +0100

    feat: update seeder ProjectSeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -15,13 +16,17 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
+        Project::truncate();
 
         for ($i = 0; $i < 25; $i++){
+            $type = Type::inRandomOrder()->first();
+
             $project = new Project();
             $project->title = $faker->sentence(3);
             $project->language_framework = $faker->randomElement(['html', 'javascript', 'php', 'laravel', 'css', 'vue', 'vuejs', 'c++', 'vite', 'react']);
             $project->visibility = $faker->randomElement(['public', 'private']);
             $project->slug = Str::of($project->title)->slug('-');
+            $project->type_id = $type->id;
 
             $project->save();
         }
