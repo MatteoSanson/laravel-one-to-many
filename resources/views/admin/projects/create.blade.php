3b642891ed586e09438bccc8ba05d90b0c62dfee
MatteoSanson <matteo.sanson@gmail.com>
2024-02-12 18:12:44 +0100

    feat: old value

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -32,7 +32,8 @@
                 <select class="form-select" aria-label="Default select example" name="type_id">
                     <option selected>Choose an option</option>
                     @foreach ($types as $type)
-                        <option value="{{ $type->id }}">{{ $type->title }}</option>
+                        <option value="{{ $type->id }}" @if (old('type_id') == $type->id) selected @endif>
+                            {{ $type->title }}</option>
                     @endforeach
                 </select>
             </div>
