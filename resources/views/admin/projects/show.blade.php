bee8ad9740cca0a9628c2f7d5df1652ca4be6f0a
MatteoSanson <matteo.sanson@gmail.com>
2024-02-12 18:50:39 +0100

    feat: add delete button

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -26,6 +26,11 @@
             <p class="card-text"><strong>Type:</strong> {{ $project->type->title }}</p>
             <p class="card-text"><strong>Language/Framework:</strong> {{ $project->language_framework }}</p>
             <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-primary btn-sm">Edit</a>
+            <form action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST" class="d-inline-block">
+                @csrf
+                @method('DELETE')
+                <input type="submit" value="Delete" class="btn btn-danger btn-sm">
+            </form>
         </div>
     </div>
 @endsection
